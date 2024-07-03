545147f9513e3fe643243343ef8426291fa32c0d
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-07-03 16:00:20 +0200

    add_project

---
+++ src/main/java/com/FireAlert/FireAlertApplication.java
@@ -0,0 +1,13 @@
+package com.FireAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FireAlert/FireAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FireAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
